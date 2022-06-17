c53f9e827bac9e4b0eebe15e134666fc1746aa39
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-06-17 11:37:15 +0800

    Made a centralised array for routes

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -5,11 +5,22 @@ import 'package:client/pages/login_page.dart';
 import 'package:client/pages/splash_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
+
+enum Pages {
+  splashPage,
+  loginPage,
+  landingPage,
+}
+
+var routesMap = <Pages, String>{
+  Pages.splashPage: '/',
+  Pages.loginPage: '/login',
+  Pages.landingPage: '/landing',
+};
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -21,10 +32,10 @@ class MyApp extends StatelessWidget {
       theme: AppTheme().buildThemeData(),
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/': (_) => const SplashPage(),
-        '/login': (_) => const LoginPage(),
-        '/landing': (_) => const LandingPage(),
-      }, 
+        routesMap[Pages.splashPage]!: (_) => const SplashPage(),
+        routesMap[Pages.loginPage]!: (_) => const LoginPage(),
+        routesMap[Pages.landingPage]!: (_) => const LandingPage(),
+      },
     );
   }
 }
