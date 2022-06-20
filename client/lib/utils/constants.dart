654f95a3217328f92b7433141fb4743de6f50ca4
Praveen Krishna <praveenkrishna0512@gmail.com>
2022-06-20 15:25:20 +0800

    Added Terraformers colors

--- client/lib/utils/constants.dart
+++ client/lib/utils/constants.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+class TerraformersConst {
+  Color lightBlue = const Color.fromARGB(255, 54, 74, 84);
+  Color mediumBlue = const Color.fromARGB(255, 16, 38, 51);
+  Color darkBlue = const Color.fromARGB(255, 5, 28, 41);
+  Color yellow = const Color.fromARGB(255, 255, 187, 0);
+}
+
 extension ShowSnackBar on BuildContext {
   void showSnackBar({
     required String message,
